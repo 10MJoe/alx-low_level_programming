b3fab38c6cec7b76acb3e2ac0d58eba76d04a64a
Micah Joe <joe.micah.e@outlook.com>
2022-04-07 09:52:39 -0700

    6 size in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio-h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("Size of a char: %d byte(s)\n", sizeof(d));
+printf("Size of an int: %d byte(s)\n", sizeof(a));
+printf("Size of a long int: %d byte(s)\n", sizeof(b));
+printf("Size of long long int: %d byte(s)\n", sizeof(c));
+printf("Size of float: %d byte(s)\n", sizeof(f));
+return (0);
+}
