057ed2cd5a72b1961093f05a8bb40aaa4e6f10ca
Micah Joe <joe.micah.e@outlook.com>
2022-04-20 06:48:50 -0700

    setup of directory and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update and interger value to 98
+ * @n: A pointer to an int that will be updated
+ * Return: 0
+ */
+void reset-to_98(int *n)
+{
+*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This readme is part of my fourth repo in the ALX SE programme.
+It contains task and activities in the 0x05 pointers arrays and strings projects
+Hope to be Successful. MJ
